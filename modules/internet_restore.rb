23abd15a5388ce9ab1d994435d01d5e1f6c40043
Kalin Staykov <k.t.staykov@gmail.com>
2017-11-18 19:29:28 +0200

    add router reboot functionality

--- modules/internet_restore.rb
+++ modules/internet_restore.rb
@@ -19,15 +19,27 @@ logger.datetime_format = '%Y-%m-%d %H:%M:%S'
 
 logger.info("Internet Restore --> Engaged!")
 
+# The next code is developed for TL-WR740N, not tested on other models.
+# It will probably not work but the idea should be the same.
+
 browser = Watir::Browser.new :chrome
 browser.goto("http://192.168.0.1")
 
 logger.info("Internet Restore --> Router #{browser.title} detected!")
 
-browser.input(id: "userName").text_field(id: "userName").set "#{ROUTER_U}"
-browser.input(id: "pcPassword").text_field(id: "pcPassword").set "#{ROUTER_P}"
-browser.label(id: "loginBtn").click
+browser.input(id: "userName").text_field(id: "userName").set "#{ROUTER_U}" # input username
+browser.input(id: "pcPassword").text_field(id: "pcPassword").set "#{ROUTER_P}" # input username
+browser.label(id: "loginBtn").click # click the login button
 
-#TODO: now that it's logged in we should navigate to the proper tab and reboot the router
+# Unfortunately this won't work:
+# dyn_string = browser.url.match(/192.168.0.1\/(.*)\/userRpm\/Index.htm/)[1]  # match the URL dynamic string
+# browser.goto("http://192.168.0.1/#{dyn_string}/userRpm/DateTimeCfgRpm.htm")
+# browser.goto("http://192.168.0.1/#{dyn_string}/userRpm/SysRebootRpm.htm")
+# I'll have to match and click or I'll get this nasty message: "You have no authority to access this router!"
+
+browser.frameset.frame(name: "bottomLeftFrame").body.a(id: "a48").click # Click on "System Tools"
+browser.frameset.frame(name: "bottomLeftFrame").body.a(id: "a54").click # Click on "System Tools -> Reboot"
+browser.frameset.frame(name: "mainFrame").body.input(name: "Reboot").click # Click the Reboot button
+browser.alert.ok # hit OK to reboot the router !
 
 binding.pry
