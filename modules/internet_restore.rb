2eea93e4a5a9c19e01ffc5eedbc0650a05aab8f9
Kalin Staykov <k.t.staykov@gmail.com>
2017-11-18 16:57:01 +0200

    add some router automation via a browser

--- modules/internet_restore.rb
+++ modules/internet_restore.rb
@@ -4,6 +4,12 @@
 
 require 'pry'
 require 'logger'
+require 'watir'
+
+# Credentials
+
+ROUTER_U = ENV['router_user']
+ROUTER_P = ENV['router_pass']
 
 # Logger settings
 
@@ -13,3 +19,15 @@ logger.datetime_format = '%Y-%m-%d %H:%M:%S'
 
 logger.info("Internet Restore --> Engaged!")
 
+browser = Watir::Browser.new :chrome
+browser.goto("http://192.168.0.1")
+
+logger.info("Internet Restore --> Router #{browser.title} detected!")
+
+browser.input(id: "userName").text_field(id: "userName").set "#{ROUTER_U}"
+browser.input(id: "pcPassword").text_field(id: "pcPassword").set "#{ROUTER_P}"
+browser.label(id: "loginBtn").click
+
+#TODO: now that it's logged in we should navigate to the proper tab and reboot the router
+
+binding.pry
