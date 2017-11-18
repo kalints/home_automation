9e8033b3ab366bf29f36214a3edb219e2e03910a
Kalin Staykov <k.t.staykov@gmail.com>
2017-11-18 14:11:47 +0200

    update new Internet module

--- modules/internet_check.rb
+++ modules/internet_check.rb
@@ -1,2 +1,24 @@
 #!/usr/bin/env ruby
 
+require 'pry'
+require 'resolv'
+require 'logger'
+
+# Logger settings
+
+logger = Logger.new(STDOUT)
+logger.level = Logger::INFO
+logger.datetime_format = '%Y-%m-%d %H:%M:%S'
+
+def check_resolv
+	begin
+		p Resolv.getaddress('google.com')
+		return true
+	rescue
+		return false
+	end
+end
+
+check_resolv or logger.fatal("Internet --> unable to resolve URLs - check DNS!")
+
+# binding.pry
