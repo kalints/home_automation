230253e8b54f7d77477ef543ef7f19d93de20207
Kalin Staykov <k.t.staykov@gmail.com>
2017-11-18 13:44:10 +0200

    update logger messages

--- main.rb
+++ main.rb
@@ -2,3 +2,4 @@
 
 # WiFi Checks
 load "modules/wifi_check.rb"
+load "modules/internet_check.rb"
--- modules/wifi_check.rb
+++ modules/wifi_check.rb
@@ -13,27 +13,27 @@ logger.datetime_format = '%Y-%m-%d %H:%M:%S'
 
 os_check = %x{uname}.strip
 if os_check == "Darwin"
-  logger.info("MacOS detected")
+  logger.info("WiFi --> MacOS detected")
 else
-  logger.fatal("Sorry, I cannot run on your system.")
+  logger.fatal("WiFi --> Sorry, I cannot run on your system.")
   exit(0)
 end
 
-logger.info("Running wifi_check...")  
+logger.info("WiFi --> Running wifi_check...")  
 
 wifi_connected = %x{/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I}.match(/\ SSID: (.*)/)[1]
 
 if wifi_connected == ENV['wifi_net']
-  logger.info("WiFi OK")
+  logger.info("WiFi --> WiFi OK")
 else
-  logger.warn("WiFi connected to wrong network!")
+  logger.warn("WiFi --> WiFi connected to wrong network!")
   if ENV['wifi_pass'] == nil
-    logger.fatal("Cannot connect to the correct network. Set your pass to ENV['wifi_pass']!")
+    logger.fatal("WiFi --> Cannot connect to the correct network. Set your pass to ENV['wifi_pass']!")
   else
-    %x{/usr/sbin/networksetup -setairportnetwork en0 #{ENV['wifi_net']} #{ENV['wifi_pass']}} || logger.fatal("Cannot connect to correct WiFi network!")
+    %x{/usr/sbin/networksetup -setairportnetwork en0 #{ENV['wifi_net']} #{ENV['wifi_pass']}} || logger.fatal("WiFi: Cannot connect to correct WiFi network!")
     wifi_connected = %x{/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I}.match(/\ SSID: (.*)/)[1]
     if wifi_connected == ENV['wifi_net']
-      logger.info("WiFi reconnected to correct network!")
+      logger.info("WiFi --> WiFi reconnected to correct network!")
     end
   end
 end
